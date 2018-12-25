d71c1f16353fbd175a71927b7bdece76f3f8f70b
Alexander Carson <alexandercarson@Alexanders-MacBook-Pro.local>
2018-12-25 09:43:11 -0600

    altered background images

--- src/App.js
+++ src/App.js
@@ -16,42 +16,48 @@ class App extends Component {
         <div className="nav-bar-1">
           <p>WHO ARE WE?</p>
         </div>
-        <div className="content">
-          <div className="text">
-            <h1>
-              We are an all American business dedicated to helping you find the
-              right tools for the job. Whether you need one piece or a hundred,
-              you can contact us below for a free estimate. We look forward to
-              working with you!
-            </h1>
+        <div className="content-1">
+          <div className="opaqueFade">
+            <div className="text">
+              <h1>
+                We are an all American business dedicated to helping you find
+                the right tools for the job. Whether you need one piece or a
+                hundred, you can contact us below for a free estimate. We look
+                forward to working with you!
+              </h1>
+            </div>
           </div>
         </div>
         <div className="nav-bar-2">
           <p>WHAT DO WE DO?</p>
         </div>
-        <div className="content">
-          <div className="text">
-            <p>
-              We're a small team of devoted individuals with years of experience
-              working with tools. From valves to vices, we can help you find the
-              piece you're looking for!
-            </p>
+        <div className="content-2">
+          <div className="opaqueFade">
+            <div className="text">
+              <p>
+                We're a small team of devoted individuals with years of
+                experience working with tools. From valves to vices, we can help
+                you find the piece you're looking for!
+              </p>
+            </div>
           </div>
         </div>
         <div className="nav-bar-3">
           <p>CONTACT US!</p>
         </div>
-        <div className="content">
-          <div className="text">
-            <p>
-              Enter your contact information below along with a quick
-              description of what you're looking for. We'll get in touch with
-              you within 48 hours!
-            </p>
+        <div className="content-3">
+          <div className="opaqueFade">
+            <div className="text">
+              <p id="lastBox">
+                Enter your contact information below along with a quick
+                description of what you're looking for. We'll get in touch with
+                you within 48 hours!
+              </p>
+            </div>
+            <input placeholder="Email address" />
+            <br />
+            <input id="supplyInput" placeholder="Supplies needed" />
           </div>
-          <input placeholder="Email address" />
-          <br />
-          <input id="supplyInput" placeholder="Supplies needed" />
         </div>
       </div>
     );
